509074af683294b40c0d42fe5058ce4f358112dc
Edwin Pile <epilepra@gmail.com>
2023-08-08 21:53:14 -0500

    backend and frontend connecting locally

--- backend/netlify/functions/api.ts
+++ backend/netlify/functions/api.ts
@@ -2,11 +2,20 @@
 
 import express, { Router } from "express";
 import serverless from "serverless-http";
+var cors = require("cors");
 
 const api = express();
+api.use(cors());
 
 const router = Router();
-router.get("/hello", (req, res) => res.send("Hello World!"));
+router.get("/hello", (req, res) =>
+  res.send({
+    statusCode: 200,
+    response: {
+      message: "Hello World!",
+    },
+  })
+);
 
 api.use("/api/", router);
 
